d0cccd4af20fa177a3f0e6dc30f5be68d9688fe1
Unknown <kirisamedesuuu@gmail.com>
2018-10-28 04:38:41 +0700

    1st change
    +control
    +camera

---
+++ Rolling Diamond/Assets/Code/Colider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Colider : MonoBehaviour {
+
+    public PlayerController playerController;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            playerController.enabled = false;
+        }
+    }
+
+}
---
+++ Rolling Diamond/Assets/Code/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	public float movingForce = 2000f;
+
+	void FixedUpdate () {
+		if ( Input.GetKey("right") ) 
+		{
+			rb.AddForce(movingForce * Time.deltaTime, 0, 0);
+		}
+		if ( Input.GetKey("left") ) 
+		{
+			rb.AddForce(-movingForce * Time.deltaTime, 0, 0);
+		}
+        if ( Input.GetKey("up"))
+        {
+            rb.AddForce(0, 0, movingForce * Time.deltaTime);
+        }
+        if (Input.GetKey("down"))
+        {
+            rb.AddForce(0, 0, -movingForce * Time.deltaTime);
+        }
+    }
+}
